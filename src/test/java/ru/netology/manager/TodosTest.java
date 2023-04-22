a70698f2b7790a24dd0ed0dd3eb00ef0928b02ed
Igor <TimoshenkoII@yandex.com>
2023-04-22 18:12:43 +0300

    Changes commit

--- src/test/java/ru/netology/manager/TodosTest.java
+++ src/test/java/ru/netology/manager/TodosTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class TodosTest {
 
-    // Доработка "Новые версии рецепта"
+    // Доработка
 
     @Test
     public void testingTheSearchRevisionTestFirst() {
